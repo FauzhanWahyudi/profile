f941d6cc1a48f078282848a7a95eca1501c5c9df
FauzhanWahyudi <fauzhanwahyudi@gmail.com>
2025-02-04 22:59:13 +0700

    feat: add image

--- profile-nextjs/next.config.mjs
+++ profile-nextjs/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/profile-nextjs", // Match your project folder inside the repo
+  assetPrefix: "/profile-nextjs/",
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
